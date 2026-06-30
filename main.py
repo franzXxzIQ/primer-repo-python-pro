f22bc4159f4cef9da985a90d3d9359de6e23f7c0
franzXxzIQ <hunzicker40@gmail.com>
2026-06-29 21:07:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hola mundo")
+meme_dict = { 
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    ("no encontramos ese dato")
